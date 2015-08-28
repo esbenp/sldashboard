448e1269b2899579a54495b399231787dbbcc48e
Mark Stoumann <admin@spotonlive.dk>
2015-08-28 14:56:04 +0200

    working on layout

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,61 +3,52 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-9">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <span class="lead">Stats</span>
                 </div>
                 <div class="panel-body">
-                    Panel content
+                    GRAPH
                 </div>
-                <div class="panel-footer">Panel footer</div>
+                <div class="panel-footer">Bla bla bla</div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="panel panel-default">
+        <div class="col-md-3">
+            <div class="panel panel-danger">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <span class="lead">Latest Commits</span>
+                    <span class="label label-danger pull-right">Danger</span>
                 </div>
                 <div class="panel-body">
-                    Panel content
+                    <b>Mark Stoumann</b><br />
+                    Commit info ... bla bla bla<hr />
+                    <b>Mark Stoumann</b><br />
+                    Commit info ... bla bla bla<hr />
+                    <b>Nikolaj Petersen</b><br />
+                    Commit info ... bla bla bla<hr />
+                    <b>Nikolaj Petersen</b><br />
+                    Commit info ... bla bla bla<hr />
+                    <b>Mark Stoumann</b><br />
+                    Commit info ... bla bla bla
                 </div>
-                <div class="panel-footer">Panel footer</div>
+                <div class="panel-footer clearfix">
+                    <div class="pull-left">
+                        <i class="fa fa-github"></i> <span class="text-muted">spotonlive/sldashboard</span>
+                    </div>
+                    <div class="pull-right">
+                        <i class="fa fa-clock-o"></i> 28-08-15 13:43
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
+
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
-                </div>
                 <div class="panel-body">
-                    Panel content
+                    <span class="lead">Andet tis</span>
                 </div>
-                <div class="panel-footer">Panel footer</div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                <div class="panel-footer">
+                    <i class="fa fa-home"></i> Panel footer
                 </div>
-                <div class="panel-body">
-                    Panel content
-                </div>
-                <div class="panel-footer">Panel footer</div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
-                </div>
-                <div class="panel-body">
-                    Panel content
-                </div>
-                <div class="panel-footer">Panel footer</div>
             </div>
         </div>
     </div>
